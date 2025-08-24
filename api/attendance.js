f3671f2da2ea3b3ba306c3a66b1c58d6cfdef4b9
명노준 <nj@myeongnojun-ui-Macmini.local>
2025-08-25 04:13:35 +0900

    🔧 Vercel API 설정 수정 - CommonJS 형식으로 변경

--- api/attendance.js
+++ api/attendance.js
@@ -18,7 +18,7 @@ db.serialize(() => {
   )`);
 });
 
-export default function handler(req, res) {
+module.exports = function handler(req, res) {
   // CORS 헤더 설정
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
